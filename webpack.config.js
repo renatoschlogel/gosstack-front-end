6231aedb118e1ffb2b0141997defd7916a0415fe
Renato W Schlogel <renato.s@outlook.com>
2020-04-12 01:40:28 -0300

    criarção do projeto react, configuração do babel e webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+		devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: [
+                            '@babel/preset-env', 
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }   
+        ]
+    }
+}

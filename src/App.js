6faa51ef259f7d0916205a16b7e676a3632d9aa9
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-04-14 23:06:27 -0300

    consumindo a api (backend) com axios e efetuando listagem dos projetos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 
 import './App.css';
-import imagem from './assets/certificado.png';
 
 import Header from "./components/Header";
 
+import api from "./services/api";
+
 function App() {
 
-    const [projects, setProjects] = useState(['Projeto React', 'Projeto Mobile']);
+    const [projects, setProjects] = useState([]);
 
-console.log(projects);
+    useEffect(() => {
+      api.get('projects').then(response => {
+        setProjects(response.data);
+      });
+    }, []);
 
     function handleAddProject() {
-        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+        // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
     }
     return (<>
               <Header title="Projects"/> 
-              <img width={100}src={imagem}/>
-
+          
               <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
               </ul>
 
               <button type="button" onClick={handleAddProject}>Adicionar Projero</button>
